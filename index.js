5d606baffac640212e27646492644f328c3b1c58
marianawal <160696201+marianawal@users.noreply.github.com>
2024-04-08 12:01:35 +0500

    commit to first

--- index.js
+++ index.js
@@ -4,3 +4,30 @@ let num1 = 7;
 let num2 = 9;
 console.log(7 + 9);
 export {};
+import inquirer from "inquirer";
+const answer = await inquirer.prompt([
+    { message: "enter firstnumber", type: "number", name: "firstnumber" },
+    { message: "enter secondnumber", type: "number", name: "secondnumber" },
+    {
+        message: "select one of the operators to perform action",
+        type: "list",
+        name: "operator",
+        choices: ["addition", "subtraction", "multiplication", "division"],
+    },
+]);
+// conditional statements
+if (answer.operator === "addition") {
+    console.log(answer.firstnumber + answer.secondnumber);
+}
+else if (answer.operators === "subtraction") {
+    console.log(answer.firstnumber - answer.secondnumber);
+}
+else if (answer.operators === "multiplication") {
+    console.log(answer.firstnumber * answer.secondnumber);
+}
+else if (answer.operators === "division") {
+    console.log(answer.firstnumber / answer.secondnumber);
+}
+else {
+    console.log("please select valid operator");
+}
